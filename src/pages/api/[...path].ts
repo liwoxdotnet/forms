7fbc377a00035ba1aebfd2e99d5a65c9b10ab0af
Armstrong Uzoagwa <liwoxfreelance@gmail.com>
2026-07-06 14:22:25 +0100

    [...path].ts file edit

--- src/pages/api/[...path].ts
+++ src/pages/api/[...path].ts
@@ -2,8 +2,13 @@ import worker from "../../../worker/index";
 
 export const prerender = false;
 
-export async function ALL(context: any) {
+async function handle(context: any) {
   const env = context.locals.runtime.env;
-
   return worker.fetch(context.request, env);
 }
+
+export const GET = handle;
+export const POST = handle;
+export const PATCH = handle;
+export const DELETE = handle;
+export const OPTIONS = handle;
